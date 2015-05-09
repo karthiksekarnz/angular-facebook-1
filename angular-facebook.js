3690b3cbba1feac759c62c447bbfae88369f122d
Paulo Cesar <pocesar@users.noreply.github.com>
2015-05-09 14:51:56 -0300

    Update angular-facebook.js
    requestAnimationFrame

--- angular-facebook.js
+++ angular-facebook.js
@@ -1,10 +1,12 @@
-angular.module('FB', []).directive('fbLike', function($timeout) {
+angular
+.module('FB', [])
+.directive('fbLike', ['$window', function($window) {
     return {
       restrict: 'A',
       scope: {},
-      template: "<div class=\"fb-like-box\" data-href=\"{{page}}\" data-width=\"{{width}}\" data-show-faces=\"{{faces}}\" data-height=\"{{height}}\" data-stream=\"{{stream}}\" data-header=\"{{header}}\"></div>",
+      template: '<div class="fb-like-box" data-href="{{page}}" data-width="{{width}}" data-show-faces="{{faces}}" data-height="{{height}}" data-stream="{{stream}}" data-header="{{header}}"></div>',
       link: function($scope, $element, $attrs) {
-        var working, _ref, _ref1;
+        var _ref, _ref1, requested = false;
 
         $scope.page = $attrs.fbLike;
         $scope.height = (_ref = $attrs.fbHeight) != null ? _ref : '550';
@@ -12,20 +14,23 @@ angular.module('FB', []).directive('fbLike', function($timeout) {
         $scope.stream = $attrs.fbStream != null ? $attrs.fbStream : 'true';
         $scope.header = $attrs.fbHeader != null ? $attrs.fbHeader : 'false';
         $scope.width = (_ref1 = $attrs.fbWidth) != null ? _ref1 : $element.parent().width();
-        working = false;
         
-        $(window).on('resize', function() {
-          if (!working) {
-            working = true;
-            $timeout(function() {
-              $scope.width = $element.parent().width();
-              $timeout(function() {
-                return FB.XFBML.parse($element[0]);
-              }, 50, false);
-              working = false;
-            }, 10);
-          }
-        });
+        function request(){
+            if (!requested) {
+                requestAnimationFrame(update);
+                requested = true;
+            }
+        }
+        
+        function update(){
+          $scope.$evalAsync(function() {
+            $scope.width = $element.parent().width();
+            FB.XFBML.parse($element[0]);
+            requested = false;
+          }, 50, false);
+        }
+
+        $window.on('resize', request);
       }
     };
-  });
+}]);
