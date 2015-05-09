af656a1f4ffbe7b053629449b8dd29129b7cf49d
Paulo Cesar <pocesar@users.noreply.github.com>
2015-05-09 15:15:49 -0300

    Update angular-facebook.js

--- angular-facebook.js
+++ angular-facebook.js
@@ -1,19 +1,24 @@
 angular
 .module('FB', [])
-.directive('fbLike', ['$window', function($window) {
+.directive('fbPage', ['$window', function($window) {
     return {
       restrict: 'A',
       scope: {},
-      template: '<div class="fb-like-box" data-href="{{page}}" data-width="{{width}}" data-show-faces="{{faces}}" data-height="{{height}}" data-stream="{{stream}}" data-header="{{header}}"></div>',
+      template: '<div class="fb-page" data-href="{{page}}" data-hide-cover="{{hide}}" data-width="{{width}}" data-show-facepile="{{faces}}" data-height="{{height}}" data-show-posts="{{posts}}"></div>',
       link: function($scope, $element, $attrs) {
-        var _ref, _ref1, requested = false;
-
-        $scope.page = $attrs.fbLike;
-        $scope.height = (_ref = $attrs.fbHeight) != null ? _ref : '550';
-        $scope.faces = $attrs.fbFaces != null ? $attrs.fbFaces : 'false';
-        $scope.stream = $attrs.fbStream != null ? $attrs.fbStream : 'true';
-        $scope.header = $attrs.fbHeader != null ? $attrs.fbHeader : 'false';
-        $scope.width = (_ref1 = $attrs.fbWidth) != null ? _ref1 : $element.parent().width();
+        var requested = false;
+        
+        $scope.height = '550';
+        $scope.faces = 'false';
+        $scope.posts = 'true';
+        $scope.hide = 'false';
+        $scope.width = $element.parent().width();
+        
+        ['page','faces','height','width','hide','posts'].forEach(function(name){
+            $attrs.$observe('fb' + name.charAt(0).toUpperCase() + name.slice(1), function(val){
+                $scope[name] = val;
+            });
+        });
         
         function request(){
             if (!requested) {
@@ -23,8 +28,8 @@ angular
         }
         
         function update(){
+          $scope.width = $element.parent().width();
           $scope.$evalAsync(function() {
-            $scope.width = $element.parent().width();
             FB.XFBML.parse($element[0]);
             requested = false;
           }, 50, false);
